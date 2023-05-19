387bfcedaed8bb85d539cfbbdbbf55bb87df487d
Alexis  BERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2023-05-19 15:28:46 +0200

    installation router link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+	import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <router-link to="/HeaderPAge.vue">Home</router-link> 
   <h1>Lightbrary</h1>
 </template>
