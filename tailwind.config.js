ad47c362307eda6309d75c988929809f3763b51b
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-05-20 17:26:06 +0200

    Ajout colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "Secondary1(Gold)": "#ecb365",
+      "Primary2(White)": "#ffffff",
+      "Primary1(Black)": "#0e0e0e",
+      "Secondary2(Beige)": "#f3d1a2",
+      "Secondary3": "#f7e0c1",
+      "Gray1": "#d3d3d3",
+      "Red": "#ff1f00",
+    },
     extend: {},
   },
   plugins: [],
