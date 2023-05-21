a3804aff12c13f7e030b90acebcbe3254e9cc380
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-05-21 18:31:18 +0200

    Insertion banniere

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
   import { RouterLink, RouterView } from 'vue-router'
   import HeaderPage from '@/components/HeaderPage.vue'
+  import HomePage from '@/pages/homepage.vue'
 </script>
 
 <template>
   <HeaderPage />
   <RouterLink to="/about">About</RouterLink>
   <main>
+    <HomePage/>
     <!-- Suspense sera utile pour charger les données (await) -->
     <Suspense>
       <!-- Affiche la page -->
---
+++ src/pages/homepage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <img src="public\img\Banniere.webp" alt="Banniere" class="relative">
+    object cover 
+</template>
