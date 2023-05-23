9bc538f921afb256b467998836a261370b690156
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-05-23 16:48:51 +0200

    Changement éléments absolute en éléments relatifs

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -6,11 +6,11 @@ import IconeAffinite from '@/components/icons/Affinite.vue'
     <div class="w-40 h-[261px]">
         <img
             src="public\img\Anneaux.webp"
-            class="w-40 h-[261px] absolute left-[26.5px] top-[570.5px] object-cover border border-Primary2(White)"
+            class="w-40 h-[261px] relative object-cover border border-Primary2(White)"
         />
         
         <div class="bg-Primary1(Black)">
-            <p class="w-[33px] absolute left-[149px] top-[594px] text-lg text-left">87%</p>
+            <p class="w-[33px] relative text-lg text-left font">87%</p>
             <IconeAffinite/>
         </div>
     </div>
