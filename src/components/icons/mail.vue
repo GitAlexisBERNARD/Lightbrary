be0f5cacc7f4794bc7fb1cde3425389b6bf7eb94
mpoilboi-iut90 <114138446+mpoilboi-iut90@users.noreply.github.com>
2023-05-24 19:24:26 +0200

    Update mail.vue

--- src/components/icons/mail.vue
+++ src/components/icons/mail.vue
@@ -1,3 +1,3 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+    <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.16671 5H25.8334C27.2542 5 28.4167 6.125 28.4167 7.5V22.5C28.4167 23.875 27.2542 25 25.8334 25H5.16671C3.74587 25 2.58337 23.875 2.58337 22.5V7.5C2.58337 6.125 3.74587 5 5.16671 5Z" stroke="#ECB365" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M28.4167 7.5L15.5 16.25L2.58337 7.5" stroke="#ECB365" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
 </template>
