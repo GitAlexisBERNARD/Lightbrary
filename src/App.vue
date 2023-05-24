5824bdf98596b8a06b11676550f4fcce15c53258
mpoilboi-iut90 <114138446+mpoilboi-iut90@users.noreply.github.com>
2023-05-24 19:17:45 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { RouterLink, RouterView } from 'vue-router'
   import HeaderPage from '@/components/HeaderPage.vue'
+  import FooterPage from '@/components/FooterPage.vue'
 </script>
 
 <template>
