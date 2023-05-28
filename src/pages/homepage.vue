8dc6dabccf1c052f8fc76de2bf0048d0e6bcd250
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-05-28 12:41:37 +0200

    Modif Home

--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -5,7 +5,7 @@ import IconSearch from '@/components/icons/search.vue'
 
 <template>
     <main class="bg-Primary1(Black) pt-10">
-        <div class="w-[333px] h-14 relative flex items-center mx-auto px-8 rounded-[20px] bg-Primary1(Black) border-[1px] border-Secondary1(Gold)">
+        <div class="w-[333px] h-14 relative flex items-center mx-auto px-8 rounded-[20px] bg-Primary1(Black) border-[1px] border-Secondary1(Gold) lg:w-[666px]">
             <IconSearch class="mr-5"/>
             <p class="relative font-text text-Primary2(White)">
                 Recherche un film,une série...
