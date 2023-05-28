623b7c6cc9fe0096e641b85c060a7d2b2cb7caf1
Alexis BERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2023-05-28 13:45:14 +0200

    correction erreur script setup carrousel

--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -1,43 +1,40 @@
-<script setup lang="ts">
+<script lang="ts">
 import HomeCard from '@/components/HomeCard.vue';
-import IconSearch from '@/components/icons/search.vue'
+import IconSearch from '@/components/icons/Search.vue';
 import CarrouselSlide from '@/components/CarrouselSlide.vue';
-import Carrousel from '@/components/Carrousel.vue'
+import Carrousel from '@/components/Carrousel.vue';
 
 export default {
-    data () {
-        return {
-            slides [
-                ''
-            ]
-        },
-    }
-    components : {
-        Carrousel,
-        CarrouselSlide,
-    }
-}
+  data() {
+    return {
+      slides: ['']
+    };
+  },
+  components: {
+    Carrousel,
+    CarrouselSlide
+  }
+};
 </script>
 
 <template>
-    <main class="bg-Primary1(Black) pt-10">
-        <div class="w-[333px] h-14 relative flex items-center mx-auto px-8 rounded-[20px] bg-Primary1(Black) border-[1px] border-Secondary1(Gold) lg:w-[666px]">
-            <IconSearch class="mr-5"/>
-            <p class="relative font-text text-Primary2(White)">
-                Recherche un film,une série...
-            </p>
-        </div>
-        <HomeCard/>
-        <div>
-            <carrousel>
-                <carrousel-slide v-for="(slide, index) in slides" 
-                    :key="slide" 
-                    :index="index"
-                    :visibleSlide="visibleSlide"
-                    >
-                    <HomeCard/>
-                </carrousel-slide>
-            </carrousel>
-        </div>
-    </main>
+  <main class="bg-Primary1(Black) pt-10">
+    <div class="w-[333px] h-14 relative flex items-center mx-auto px-8 rounded-[20px] bg-Primary1(Black) border-[1px] border-Secondary1(Gold) lg:w-[666px]">
+      <IconSearch class="mr-5"/>
+      <p class="relative font-text text-Primary2(White)">
+        Recherche un film, une série...
+      </p>
+    </div>
+    <HomeCard/>
+    <div>
+      <Carrousel>
+        <CarrouselSlide v-for="(slide, index) in slides" 
+                        :key="slide" 
+                        :index="index"
+        >
+          <HomeCard/>
+        </CarrouselSlide>
+      </Carrousel>
+    </div>
+  </main>
 </template>
