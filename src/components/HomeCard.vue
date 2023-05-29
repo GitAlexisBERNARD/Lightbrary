3f38ee9a6d3f2ecc2d52a2a47dbe2fed6d9c0e81
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-05-29 16:38:23 +0200

    Reglage position affinité

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -3,13 +3,13 @@ import IconeAffinite from '@/components/icons/Affinite.vue'
 </script>
 
 <template>
-    <div class="flex relative min-w-[160px] max-w-[201px] col-span-2 border border-Primary2(White)">
+    <div class="relative min-w-[160px] max-w-[201px] col-span-2 border border-Primary2(White)">
         <img
             src="public/img/Anneaux.webp"
             class="w-full h-full z-10 object-cover"
         />
         
-        <div class="w-[69px] h-[27px] absolute top-3 left-[91px] z-20 flex justify-center space-x-2 rounded-tl-[20px] rounded-bl-[20px] bg-Primary1(Black) border-r border-Primary2(White)">
+        <div class="w-[69px] h-[27px] absolute top-5 right-0 z-20 flex justify-center space-x-2 rounded-tl-[20px] rounded-bl-[20px] bg-Primary1(Black) border-r border-Primary2(White)">
             <IconeAffinite class="w-[14.24px] h-[21px]"/>
             <p class="text-Primary2(White) text-left text-18px font-text">87%</p>
         </div>
