221ec6bab6e7b917e89f57b69b48f32050b389f2
mpoilboi-iut90 <114138446+mpoilboi-iut90@users.noreply.github.com>
2023-06-05 00:13:08 +0200

    Update FooterPage.vue

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -12,20 +12,22 @@
 </script>
 
 <template>
-    <footer class="space-y-10 pt-20 max-w-screen-xl m-auto lg:grid-cols-2 lg-gap-8 lg:space-y-0 bg-Sky">
-        <nav class="flex items-center justify-center">
-            <IconLogo class="mx-8 lg-col-span-1 w-48"/>
-                <nav class="text-Primary2(White) font-ubuntu">
+    <footer class="text-Primary2(White) font-ubuntu bg-Sky">
+        <nav class="flex items-center justify-center lg:hidden mt-[-60px]">
+            <a href="#">
+                <IconLogo class="w-48"/>
+            </a>
+                <nav>
                     <div class="text-center">
-                        <h3>A propos</h3>
+                        A propos
                     </div>
                     
-                    <div class="flex ml-7 items-center gap-4 pt-6">
-                        <button class="relative z-10 flex h-5 w-8 flex-col justify-between" 
+                    <div class="ml-7 pt-6">
+                        <button  
                         @click="showOverlay = true">
                             Contact
                         </button>
-                        <nav class="fixed inset-0 flex z-20"
+                        <nav class="fixed inset-0 flex"
                             :class="{ 'hidden': !showOverlay }">
                             <div class="fixed inset-0 bg-Primary1(Black) opacity-full"></div>
                             <div class="absolute top-0 right-0 m-4">
@@ -34,27 +36,27 @@
                                     <IconX />
                                 </button>
                             </div>
-                            <div class="ml-8 z-30">
+                            <div class="ml-12 z-10">
                                 <h1 class="text-Primary2(White) pt-12 pb-10">Contactez nous</h1>
                                     <h6 class="text-Secondary1(Gold)">Email</h6>
                                     <div class="mt-2">
                                         <input type="text" placeholder="exemple@gmail.com" class="px-3 py-2 border border-Secondary1(Gold)
-                                        rounded-md w-64 focus:outline-none focus:ring-Primary2 focus:border-Primary2 bg-Primary1(Black)">
+                                        rounded-md w-80 focus:outline-none focus:ring-Primary2 focus:border-Primary2 bg-Primary1(Black)">
                                     </div>
                                     <h6 class="text-Secondary1(Gold) pt-4">Nom</h6>
                                     <div class="mt-2">
                                         <input type="text" placeholder="Nom" class="px-3 py-2 border border-Secondary1(Gold)
-                                        rounded-md w-64 focus:outline-none focus:ring-Primary2 focus:border-Primary2 bg-Primary1(Black)">
+                                        rounded-md w-80 focus:outline-none focus:ring-Primary2 focus:border-Primary2 bg-Primary1(Black)">
                                     </div>
                                     <h6 class="text-Secondary1(Gold) pt-4">Prénom</h6>
                                     <div class="mt-2">
                                         <input type="text" placeholder="Prénom" class="px-3 py-2 border border-Secondary1(Gold)
-                                        rounded-md w-64 focus:outline-none focus:ring-Primary2 focus:border-Primary2 bg-Primary1(Black)">
+                                        rounded-md w-80 focus:outline-none focus:ring-Primary2 focus:border-Primary2 bg-Primary1(Black)">
                                     </div>
                                     <h6 class="text-Secondary1(Gold) pt-4">Commentaire</h6>
                                     <div class="mt-2">
                                         <input type="text" placeholder="Détails" class="px-3 py-2 border border-Secondary1(Gold)
-                                        rounded-md w-64 h-[130px] focus:outline-none focus:ring-Primary2 focus:border-Primary2 
+                                        rounded-md w-80 h-[130px] focus:outline-none focus:ring-Primary2 focus:border-Primary2 
                                         bg-Primary1(Black) pb-20">    
                                     </div>
                                 <nav class="flex justify-center pt-10">
@@ -72,21 +74,82 @@
                     </div>
                 </nav>
         </nav>
-        <div class="flex justify-center pt-10">
-            <ul class="flex space-x-4 mb-14">
-                <li class="w-1/2 flex-auto">
+        <div class="flex justify-center lg:hidden">
+            <ul class="flex space-x-12 mb-14 mt-[-70px]">
+                <li>
                     <a href="#"><IconFacebook /></a>
                 </li>
-                <li class="w-1/2 flex-auto">
+                <li>
                     <a href="#"><IconInstagram /></a>
                 </li>
-                <li class="w-1/2 flex-auto">
+                <li>
                     <a href="#"><IconTwitter /></a>
                 </li>
-                <li class="w-1/2 flex-auto">
+                <li>
                     <a href="#"><IconMail /></a>
                 </li>
             </ul>
         </div>
+
+        <nav class="hidden lg:grid lg:grid-cols-5 pt-8 pl-20 gap-44">
+            <div class="lg:col-start-1">Contact
+                <ul class="text-sm pt-4">
+                    <li>Département MMI</li>
+                    <li>4 place Tharradin,</li>
+                    <li>25200 Montbéliard</li>
+                    <li>France</li>
+                    <li class="pt-4">Contact@gmail.com</li>
+                    <li>03 81 99 47 34</li>
+                </ul>
+            </div>
+            <div class="lg:col-start-2">Mentions légales
+                <ul class="text-sm pt-4">
+                    <li>Politique de confidentialité</li>
+                    <li class="pt-2">Conditions d'utilisation</li>
+                    <li class="pt-2">RGPD</li>
+                    <li class="pt-2">Mentions légales</li>
+                    <li class="pt-2">Plan du site</li>
+                </ul>
+            </div>
+            <div class="lg:col-start-3">
+                <a href="#">
+                    <IconLogo class="w-40 mt-[-150px]"/>
+                </a>
+                <div class="flex justify-center lg:col-start-3 mt-[-110px]">
+                    <ul class="flex space-x-8 mb-10">
+                        <li>
+                            <a href="#"><IconFacebook /></a>
+                        </li>
+                        <li>
+                            <a href="#"><IconInstagram /></a>
+                        </li>
+                        <li>
+                            <a href="#"><IconTwitter /></a>
+                        </li>
+                        <li>
+                            <a href="#"><IconMail /></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="lg:col-start-4">Lightbrary
+                <ul class="text-sm pt-4">
+                    <li>Accueil</li>
+                    <li class="pt-2">Films</li>
+                    <li class="pt-2">Séries</li>
+                    <li class="pt-2">Livres</li>
+                    <li class="pt-2">Musiques</li>
+                </ul>
+            </div>
+            <div class="lg:col-start-5">A propos
+                <ul class="text-sm pt-4">
+                    <li>Nous</li>
+                    <li class="pt-2">Carrière</li>
+                    <li class="pt-2">FAQs</li>
+                    <li class="pt-2">Équipe</li>
+                </ul>
+            </div>
+        </nav>
+
     </footer>
 </template>
