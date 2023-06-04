def79261c498afad24d3807290f1e7bf00aa7055
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-06-04 11:38:01 +0200

    Page inscription desktop

--- src/pages/inscription.vue
+++ src/pages/inscription.vue
@@ -1,27 +1,3 @@
-<template>
-  <main>
-    <h1>Connexion</h1>
-    <div>
-      <label for="name">Nom d'utilisateur:</label>
-      <input type="text" id="name" />
-      <p></p>
-      <label for="login">Adresse Mail:</label>
-      <input type="email" id="login" />
-      <p></p>
-      <label for="passwd">Mot de passe:</label>
-      <input type="password" id="passwd" />
-      <p></p>
-      <label for="confirmPasswd">Confirmer le mot de passe:</label>
-      <input type="password" id="confirmPasswd" />
-      <p></p>
-      <p id="error-message" style="color: red;"></p>
-      <hr />
-      <button @click="register">S'inscrire</button>
-      <p></p>
-    </div>
-  </main>
-</template>
-
 <script>
 import { useRouter } from 'vue-router';
 import PocketBase from 'pocketbase';
@@ -69,3 +45,43 @@ export default {
   },
 };
 </script>
+
+<template>
+  <main class="lg:grille_connexion bg-Primary1(Black)">
+    <div class="col-span-4 col-start-5 flex flex-col">
+      <label class="font-text font-medium text-Secondary1(Gold) text-[24px] pb-3" for="login">Email</label>
+      <input  class="bg-Primary1(Black) py-2 border border-Secondary2(Beige) font-text text-Secondary1(Gold) placeholder:text-Gray1 text-[16px] rounded-[8px] px-4" type="email" id="login" placeholder="exemple@gmail.com"/>
+      <p></p>
+    </div>
+
+    <div class="col-span-4 col-start-5 row-start-2 flex flex-col">
+      <label class="font-text font-medium text-Secondary1(Gold) text-[24px] pb-3" for="name">Nom d'utilisateur</label>
+      <input class="bg-Primary1(Black) py-2 border border-Secondary2(Beige) font-text text-Secondary1(Gold) placeholder:text-Gray1 text-[16px] rounded-[8px] px-4" type="text" id="name" placeholder="krys_film"/>
+      <p></p>
+    </div>
+
+    <div class="col-span-4 col-start-5 row-start-3 flex flex-col">
+      <label class="font-text font-medium text-Secondary1(Gold) text-[24px] pb-3" for="passwd">Mot de passe</label>
+      <input class="bg-Primary1(Black) py-2 border border-Secondary2(Beige) font-text text-Secondary1(Gold) text-[16px] rounded-[8px] px-4" type="password" id="passwd" />
+      <p></p>
+    </div>
+
+    <div class="col-span-4 col-start-5 row-start-4 flex flex-col">
+      <label class="font-text font-medium text-Secondary1(Gold) text-[24px] pb-3" for="confirmPasswd">Répétez le mot de passe</label>
+      <input class="bg-Primary1(Black) py-2 border border-Secondary2(Beige) font-text text-Secondary1(Gold) text-[16px] rounded-[8px] px-4" type="password" id="confirmPasswd" />
+      <p></p>
+      <p class="font-text" id="error-message" style="color: red;"></p>
+    </div>
+
+    <div class="col-span-4 col-start-5">
+      <p class="font-text text-Primary2(White) text-[12px]">J'accepte les conditions d'utilisation et la charte sur le respect de la vie privée.</p>
+    </div>
+
+    <button class="col-span-2 col-start-6 py-4 bg-Secondary1(Gold) font-text font-medium text-Primary1(Black) text-[20px] rounded-[8px]" @click="register">S'inscrire</button>
+    
+    <div >
+      <hr />
+      <p></p>
+    </div>
+  </main>
+</template>
