0af584ff1b007cb38917a289a005109bc497aa47
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-06-05 09:06:51 +0200

    Page Médias desktop

--- src/pages/pages/[id].vue
+++ src/pages/pages/[id].vue
@@ -2,18 +2,51 @@
 import Affinite from '@/components/icons/Affinite.vue';
 import like from '@/components/icons/like(black).vue';
 import check from '@/components/icons/check.vue'
+import HomeCard from '@/components/HomeCard.vue';
 </script>
 
 <template>
     <main class=" bg-Primary1(Black) pb-12">
         <section>
-            <header class="grille_mobile pt-7">
-                <img class="col-span-2 col-start-2 border border-Primary2(White) rounded-[20px]" src="/img/Test_Image_Page.webp" alt="Test Images">
+            <header class="grille_mobile lg:grille_profil pt-7">
+                <img class="col-span-2 col-start-2 border border-Primary2(White) rounded-[20px] lg:col-span-3 lg:col-start-1" src="/img/Test_Image_Page.webp" alt="Test Images">
 
-                <h1 class="col-span-4 font-text font-bold text-Primary2(White) text-[24px] text-center">Ready Player One</h1>
-                <h2 class="col-span-2 col-start-2 font-text font-medium text-Secondary1(Gold) text-center -mt-7">Steven Spielberg</h2>
+                <div class="col-span-4 font-text text-center lg:col-span-5 lg:col-start-4 lg:text-start ">
+                    <h1 class="font-bold text-Primary2(White) text-[24px] lg:text-[35px]">Ready Player One</h1>
+                    <h2 class="font-medium text-Secondary1(Gold) lg:text-[25px]">Steven Spielberg</h2>
 
-                <div class="col-span-4 flex justify-between font-text text-Secondary1(Gold) text-[14px]">
+                    <div class="hidden lg:flex justify-between font-text text-Secondary1(Gold) text-[16px] pt-5">
+                        <p class="border rounded-[20px] py-1 px-5">Action</p>
+                        <p class="border rounded-[20px] py-1 px-5">Aventure</p>
+                        <p class="border rounded-[20px] py-1 px-5">Science-Fiction</p>
+                    </div>
+
+                    <div class="hidden lg:flex flex-col py-5">
+                        <div class="flex items-center gap-2">
+                            <Affinite class="w-[25px]"/>
+                            <p class="font-text font-bold text-Primary2(White)">98%</p>
+                        </div>
+
+                        <div class="flex items-center gap-2">
+                            <p class="font-text text-Primary2(White) text-[14px]">Score Metacritic</p>
+                            <p class="font-text font-bold text-Primary2(White) bg-[#66CC33] p-1">100</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="hidden lg:flex col-span-2 items-end gap-1">
+                    <div class="bg-Secondary1(Gold) rounded-l-[6px] flex flex-col items-center justify-center p-2 gap-2 h-[90px]">
+                        <like/>
+                        <p class=" font-text text-Primary1(Black) text-[10px] text-center">Ajouter à ma Watchlist</p>
+                    </div>
+
+                    <div class="bg-Primary2(White) opacity-[0.90] rounded-r-[6px] flex flex-col items-center justify-center p-2 gap-2 h-[90px]">
+                        <check/>
+                        <p class=" font-text text-Primary1(Black) text-[10px] text-center">Marquer comme vu</p>
+                    </div>
+                </div>
+                
+                <div class="col-span-4 flex justify-between font-text text-Secondary1(Gold) text-[14px] lg:hidden">
                     <p class="border rounded-[20px] py-1 px-5">Action</p>
                     <p class="border rounded-[20px] py-1 px-5">Aventure</p>
                     <p class="border rounded-[20px] py-1 px-5">Science-Fiction</p>
@@ -21,7 +54,7 @@ import check from '@/components/icons/check.vue'
             </header>
 
             <article>
-                <div class="grille_mobile py-5">
+                <div class="grille_mobile py-5 lg:hidden">
                     <div class="col-span-2 flex items-center gap-2">
                         <Affinite class="w-[25px]"/>
                         <p class="font-text font-bold text-Primary2(White)">98%</p>
@@ -33,7 +66,7 @@ import check from '@/components/icons/check.vue'
                     </div>
                 </div>
 
-                <div class="grille_mobile">
+                <div class="grille_mobile lg:hidden">
                     <div class="col-span-4 bg-Secondary1(Gold) rounded-[6px] flex items-center p-2 gap-4">
                         <like/>
                         <p class=" font-text text-Primary1(Black) text-[10px]">Ajouter à ma Watchlist</p>
@@ -45,16 +78,22 @@ import check from '@/components/icons/check.vue'
                     </div>
                 </div>
 
-                <div class="grille_mobile py-5">
-                    <p class="col-span-4 font-text text-Primary2(White) text-[12px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia commodo metus, quis vulputate turpis rhoncus ac. Vivamus pulvinar aliquet mi id pretium. Maecenas ac malesuada enim. Fusce at nibh in eros rutrum fermentum at ut sapien. Vestibulum varius ligula enim, non rutrum est laoreet sit amet. Curabitur sed cursus ante, a condimentum ipsum. Nunc imperdiet hendrerit nunc, vitae tempus lacus. Donec sed est libero. Nulla eu dolor nec urna vulputate iaculis. Duis interdum quis ante eget tincidunt. Duis ultrices odio nibh, lobortis molestie ante luctus sed. Sed quis luctus massa, at viverra tellus. Mauris in rutrum purus.</p>
+                <div class="grille_mobile lg:grille_profil py-5 lg:py-20">
+                    <h2 class="hidden lg:block font-text font-bold text-Primary2(White) text-[24px] lg:col-span-3 lg:border-b lg:border-Secondary1(Gold) lg:pb-2">Histoire</h2>
+                    <p class="col-span-4 font-text text-Primary2(White) text-[12px] lg:col-span-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lacinia commodo metus, quis vulputate turpis rhoncus ac. Vivamus pulvinar aliquet mi id pretium. Maecenas ac malesuada enim. Fusce at nibh in eros rutrum fermentum at ut sapien. Vestibulum varius ligula enim, non rutrum est laoreet sit amet. Curabitur sed cursus ante, a condimentum ipsum. Nunc imperdiet hendrerit nunc, vitae tempus lacus. Donec sed est libero. Nulla eu dolor nec urna vulputate iaculis. Duis interdum quis ante eget tincidunt. Duis ultrices odio nibh, lobortis molestie ante luctus sed. Sed quis luctus massa, at viverra tellus. Mauris in rutrum purus.</p>
                 </div>
 
-                <div class="grille_mobile mt-5">
+                <div class="grille_mobile mt-5 lg:hidden">
                     <button class="col-span-2 col-start-2">
                         <RouterLink class="font-text font-medium text-Secondary1(Gold) border border-Secondary1(Gold) rounded-[8px] py-3 px-10" to="">
                             Retour
                         </RouterLink>
                     </button>
+                </div>
+
+                <div class="hidden lg:grille_profil">
+                    <h2 class="font-text font-bold text-Primary2(White) text-[24px] lg:col-span-3 lg:border-b lg:border-Secondary1(Gold) lg:pb-2">Du même genre</h2>
+                    <HomeCard class="lg:row-start-2"/>
                 </div>
             </article>
         </section>
