b652ab7b4b42ff768856527328635c1b09aacf6a
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-06-05 21:37:53 +0200

    Page création profil genres mobile

--- src/pages/creation-profil-genres.vue
+++ src/pages/creation-profil-genres.vue
@@ -2,27 +2,27 @@
 </script>
 
 <template>
-    <main>
+    <main class="bg-Primary1(Black) pb-10">
         <section>
-            <header>
-                <h1>Création profil utilisateur</h1>
-                <p>Afin de vous proposer la meilleure recommandation, pourriez-vous renseigner vos genres préférés ?</p>
+            <header class="grille_mobile">
+                <h1 class="col-span-4 font-text font-bold text-Primary2(White) text-[24px]">Création profil utilisateur</h1>
+                <p class="col-span-4 font-text text-Primary2(White) text-[15px]">Afin de vous proposer la meilleure recommandation, pourriez-vous renseigner vos genres préférés ?</p>
             </header>
 
-            <article>
-                <div>
-                    <h2>Mes styles préférés</h2>
+            <article class="grille_mobile mt-10">
+                <div class="col-span-4">
+                    <h2 class="col-span-4 font-text font-bold text-Primary2(White) text-[16px]">Mes styles préférés</h2>
 
-                    <ul>
-                        <li>Science-Fiction</li>
-                        <li>Science-Fiction</li>
-                        <li>Science-Fiction</li>
-                        <li>Science-Fiction</li>
-                        <li>Science-Fiction</li>
+                    <ul class="ml-10 font-text text-Primary2(White) text-[14px]">
+                        <li class="pt-3"><input class="w-[17px] h-[17px] accent-Secondary1(Gold)" type="checkbox" id="checkbox" v-model="checked" /> Science-Fiction</li>
+                        <li class="pt-3"><input class="w-[17px] h-[17px] accent-Secondary1(Gold)" type="checkbox" id="checkbox" v-model="checked" /> Science-Fiction</li>
+                        <li class="pt-3"><input class="w-[17px] h-[17px] accent-Secondary1(Gold)" type="checkbox" id="checkbox" v-model="checked" /> Science-Fiction</li>
+                        <li class="pt-3"><input class="w-[17px] h-[17px] accent-Secondary1(Gold)" type="checkbox" id="checkbox" v-model="checked" /> Science-Fiction</li>
+                        <li class="pt-3"><input class="w-[17px] h-[17px] accent-Secondary1(Gold)" type="checkbox" id="checkbox" v-model="checked" /> Science-Fiction</li>
                     </ul>  
                 </div>
 
-                <button>Suivant</button>
+                <button class="col-span-2 col-start-2 font-text font-medium text-Primary1(Black) bg-Secondary1(Gold) px-5 py-3 rounded-[8px] mt-6">Suivant</button>
             </article>
         </section>
     </main>
