c65045dfd408016f32bd1b42f69501eddad3fd41
mpoilboi-iut90 <marco.poilbois@edu.univ-fcomte.fr>
2023-06-05 22:38:16 +0200

    début landing-page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,6 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
+    <img src="/img/Banniere.webp">
+    Vous avez du temps et vous ne savez pas quoi faire ?
 </template>
