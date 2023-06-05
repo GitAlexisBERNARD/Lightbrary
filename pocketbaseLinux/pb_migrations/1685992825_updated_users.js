f383671ef1ff36ef66b06553bcff593449e0c6f1
GitAlexisBERNARD <alexis.bernard02@edu.univ-fcomte.fr>
2023-06-05 22:13:14 +0200

    idtogenre

---
+++ pocketbaseLinux/pb_migrations/1685992825_updated_users.js
@@ -0,0 +1,43 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("idwmyvzl")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "4nap8lbt",
+    "name": "genre",
+    "type": "json",
+    "required": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "idwmyvzl",
+    "name": "test",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("4nap8lbt")
+
+  return dao.saveCollection(collection)
+})
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,18 +1,16 @@
-<script setup lang="ts">
-  import { RouterLink, RouterView } from 'vue-router'
-  import HeaderPage from '@/components/HeaderPage.vue'
-</script>
-
-
 <template>
   <div>
     <h1>Bienvenue sur le tableau de bord</h1>
     <p v-if="userInfo">Vous êtes connecté en tant que {{ userInfo.username }}</p>
-    <button @click="pb.authStore.clear();">deconnexion</button>
+    <button @click="pb.authStore.clear()">deconnexion</button>
   </div>
 </template>
+
 <script lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import HeaderPage from '@/components/HeaderPage.vue';
 import PocketBase from 'pocketbase';
+
 var pocketbase_ip = '';
 if (process.env.NODE_ENV === 'production') {
   pocketbase_ip = '193.168.146.150:80';
@@ -28,10 +26,89 @@ export default {
       userInfo: null
     };
   },
-  mounted() {
+  async mounted() {
     const userInfo = pb.authStore.model;
     console.log(userInfo);
     this.userInfo = userInfo;
+    const genres = await this.getGenres(pb.authStore.model.data);
+    this.updateGenre(genres);
+  },
+  methods: {
+    async updateGenre(json) {
+      const userInfo = pb.authStore.model.id.toString();
+      try {
+        await pb.collection('users').update(userInfo, { 'genre': `${json}` });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getGenres(jsonData) {
+      const movieGenreApiUrl = 'https://api.themoviedb.org/3/movie/';
+      const bookGenreApiUrl = 'https://www.googleapis.com/books/v1/volumes/';
+      const musicGenreApiUrl = 'https://itunes.apple.com/lookup?id=';
+
+      const genres = {};
+
+      if (jsonData.Film) {
+        for (const movieId of jsonData.Film) {
+          const response = await fetch(`${movieGenreApiUrl}${movieId}?api_key=ab3ffc07e2a06a3122219298b0ba013b`);
+          const movieData = await response.json();
+
+          if (movieData.genres) {
+            genres[`Film_${movieId}`] = movieData.genres;
+          }
+        }
+      }
+
+      if (jsonData.Livre) {
+        for (const bookId of jsonData.Livre) {
+          const response = await fetch(`${bookGenreApiUrl}${bookId}`);
+          const bookData = await response.json();
+
+          if (bookData.volumeInfo && bookData.volumeInfo.categories) {
+            genres[`Livre_${bookId}`] = bookData.volumeInfo.categories;
+          }
+        }
+      }
+
+      if (jsonData.Musique) {
+        for (const musicId of jsonData.Musique) {
+          const response = await fetch(`${musicGenreApiUrl}${musicId}`);
+          const musicData = await response.json();
+
+          if (musicData.results && musicData.results[0] && musicData.results[0].primaryGenreName) {
+            genres[`Musique_${musicId}`] = [musicData.results[0].primaryGenreName];
+          }
+        }
+      }
+
+      const transformedGenres = {};
+
+      for (const key in genres) {
+        const [type, id] = key.split('_');
+        const genreData = genres[key];
+
+        if (!transformedGenres[type]) {
+          transformedGenres[type] = { id: [], genre: [] };
+        }
+
+        transformedGenres[type].id.push(id);
+
+        // Ajouter chaque genre séparément
+        genreData.forEach(genreItem => {
+          const genre = genreItem.name || genreItem;
+
+          if (genre.includes('&')) {
+            const separatedGenres = genre.split(' & ');
+            transformedGenres[type].genre.push(...separatedGenres);
+          } else {
+            transformedGenres[type].genre.push(genre);
+          }
+        });
+      }
+
+      return JSON.stringify(transformedGenres);
+    }
   }
 };
 </script>
