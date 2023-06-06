e942507ad7b13cd7a636bf500977c21171667a3d
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-06-06 16:35:07 +0200

    Ajout Pocketbase

---
+++ pocketbaseLinux/pb_migrations/1686006975_updated_users.js
@@ -0,0 +1,31 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "xiag5r5w",
+    "name": "imgprofile",
+    "type": "file",
+    "required": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "mimeTypes": [],
+      "thumbs": [],
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("xiag5r5w")
+
+  return dao.saveCollection(collection)
+})
