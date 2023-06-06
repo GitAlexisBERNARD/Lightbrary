0636d72f15971876de748653d90ffb5809b0dc03
Rebuffoni-Xavier <xavier.rebuffoni@edu.univ-fcomte.fr>
2023-06-06 19:21:32 +0200

    Ajout début code statique page profil

--- src/pages/profil/[id].vue
+++ src/pages/profil/[id].vue
@@ -1,3 +1,195 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import HeaderPage from '@/components/HeaderPage.vue';
+import PocketBase from 'pocketbase';
+
+var pocketbase_ip = '';
+if (process.env.NODE_ENV === 'production') {
+  pocketbase_ip = '193.168.146.150:80';
+} else {
+  pocketbase_ip = 'http://127.0.0.1:8090';
+}
+
+const pb = new PocketBase(pocketbase_ip);
+
+export default {
+  data() {
+    return {
+      userInfo: null,
+    };
+  },
+  async mounted() {
+    const userInfo = pb.authStore.model;
+    console.log(userInfo);
+    this.userInfo = userInfo;
+    const genres = await this.getGenres(pb.authStore.model.data);
+    this.updateGenre(genres);
+    if (userInfo.firstconnexion === false) {
+      await this.initwhilistdata();
+      await pb.collection('users').update(userInfo.id.toString(), { 'firstconnexion': true });
+    }
+  },
+  
+  methods: {
+    async initwhilistdata(){
+      const userInfo = pb.authStore.model.id.toString();
+      const json = JSON.stringify({ "Film": [], "Livre": [], "Musique": [] });
+        try {
+          await pb.collection('users').update(userInfo, { 'watchlist': `${json}`});
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    async updateGenre(json) {
+      const userInfo = pb.authStore.model.id.toString();
+      try {
+        await pb.collection('users').update(userInfo, { 'genre': `${json}` });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getGenres(jsonData) {
+      const movieGenreApiUrl = 'https://api.themoviedb.org/3/movie/';
+      const bookGenreApiUrl = 'https://www.googleapis.com/books/v1/volumes/';
+      const musicGenreApiUrl = 'https://itunes.apple.com/lookup?id=';
+
+      const genres = {};
+
+      if (jsonData.Film) {
+        for (const movieId of jsonData.Film) {
+          const response = await fetch(`${movieGenreApiUrl}${movieId}?api_key=ab3ffc07e2a06a3122219298b0ba013b`);
+          const movieData = await response.json();
+
+          if (movieData.genres) {
+            genres[`Film_${movieId}`] = movieData.genres;
+          }
+        }
+      }
+
+      if (jsonData.Livre) {
+        for (const bookId of jsonData.Livre) {
+          const response = await fetch(`${bookGenreApiUrl}${bookId}`);
+          const bookData = await response.json();
+
+          if (bookData.volumeInfo && bookData.volumeInfo.categories) {
+            genres[`Livre_${bookId}`] = bookData.volumeInfo.categories;
+          }
+        }
+      }
+
+      if (jsonData.Musique) {
+        for (const musicId of jsonData.Musique) {
+          const response = await fetch(`${musicGenreApiUrl}${musicId}`);
+          const musicData = await response.json();
+
+          if (musicData.results && musicData.results[0] && musicData.results[0].primaryGenreName) {
+            genres[`Musique_${musicId}`] = [musicData.results[0].primaryGenreName];
+          }
+        }
+      }
+
+      const transformedGenres = {};
+
+      for (const key in genres) {
+        const [type, id] = key.split('_');
+        const genreData = genres[key];
+
+        if (!transformedGenres[type]) {
+          transformedGenres[type] = { id: [], genre: [] };
+        }
+
+        transformedGenres[type].id.push(id);
+
+        genreData.forEach(genreItem => {
+          const genre = genreItem.name || genreItem;
+
+          if (genre.includes('&')) {
+            const separatedGenres = genre.split(' & ');
+            transformedGenres[type].genre.push(...separatedGenres);
+          } else {
+            transformedGenres[type].genre.push(genre);
+          }
+        });
+      }
+
+      return JSON.stringify(transformedGenres);
+    }
+  }
+};
+const data = {
+  "Film": {
+    "id": [
+      "333339"
+    ],
+    "genre": [
+      "Science Fiction",
+      "Adventure",
+      "Action"
+    ]
+  },
+  "Livre": {
+    "id": [
+      "LH7ziruT7ZQC",
+      "kRctEAAAQBAJ",
+      "VugRtAEACAAJ",
+      "XLVJDwAAQBAJ"
+    ],
+    "genre": [
+      "Fiction / Science Fiction / General",
+      "Fiction / Science Fiction / General",
+      "Fiction / Science Fiction / Action",
+      "Adventure",
+      "Fiction / Literary",
+      "Fiction / General"
+    ]
+  },
+  "Musique": {
+    "id": [
+      "1354248992",
+      "1454446626",
+      "1454446624",
+      "1412214020",
+      "1454446630"
+    ],
+    "genre": [
+      "Action",
+      "Adventure",
+      "Rock",
+      "R&B/Soul",
+      "Dance",
+      "Rock"
+    ]
+  },
+  "GenrePref": {
+    "genre": [
+      "Science Fiction",
+      "Adventure"
+    ]
+  }
+};
+
+let genreCounts = {};
+
+for (let category in data) {
+
+  data[category].genre.forEach(genre => {
+
+    genreCounts[genre] = genreCounts[genre] ? genreCounts[genre] + 1 : 1;
+  });
+}
+
+
+let genres = Object.keys(genreCounts).map(genre => [genre, genreCounts[genre]]);
+
+
+genres.sort((a, b) => b[1] - a[1]);
+
+let topFiveGenres = genres.slice(0, 5);
+
+console.log(topFiveGenres);
+
+</script>
+
 <script setup lang="ts">
 import like from '@/components/icons/like.vue';
 import stat from '@/components/icons/stat.vue';
@@ -12,7 +204,7 @@ import HomeCard from '@/components/HomeCard.vue'
     <main class="bg-Primary1(Black)">
         <section>
             <header class="grille_mobile lg:grille_profil">
-                <h1 class="hidden lg:block font-text font-bold text-Primary2(White) text-[27px]">krys_film</h1>
+                <h1 class="hidden lg:block font-text font-bold text-Primary2(White) text-[27px]">{{ userInfo.username }}</h1>
 
                 <img class="rounded-[9999px] col-span-4 border-[5px] border-Primary2(White) my-5 lg:rounded-[0px] lg:border-none lg:col-span-2 lg:col-start-1 lg:my-0" src="../../../public/img/Test_Profil.webp" alt="Test">
 
@@ -57,7 +249,7 @@ import HomeCard from '@/components/HomeCard.vue'
                 </div>
 
                 <div class="grille_mobile lg:grille_profil py-7">
-                    <RouterLink class="col-span-3 flex gap-3 items-center font-text text-Primary2(White) lg:hidden" to="">
+                    <RouterLink class="col-span-3 flex gap-3 items-center font-text text-Primary2(White) lg:hidden" to="/profil/statistique">
                         <stat/>
 
                         Mes statistiques
@@ -103,11 +295,11 @@ import HomeCard from '@/components/HomeCard.vue'
                 </div>
 
                 <div class="grille_mobile pb-7 lg:hidden">
-                    <RouterLink class="col-span-3 flex gap-3 items-center font-text text-Primary2(White)" to="">
+                    <button class="col-span-3 flex gap-3 items-center font-text text-Primary2(White)" @click="pb.authStore.clear()">
                         <deconnexion/>
 
                         Déconnexion
-                    </RouterLink>
+                    </button>
 
                     <RouterLink class="col-start-4 flex justify-end" to="">
                         <chevronRightVue class="stroke-Secondary1(Gold)"/>
